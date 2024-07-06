389948795c717375fa76483993fd2f6527c646f1
12kishansingh <ks888singh@gmail.com>
2024-07-07 00:00:53 +0530

    done with auth screen now working on send data to firebase

--- lib/auth.dart
+++ lib/auth.dart
@@ -9,54 +9,117 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  final _form = GlobalKey<FormState>();
+  var _isLogin = true;
+  var _enteredEmail = '';
+  var _eneteredPassword = '';
+
+  void _sumbit() {
+    // tirgger my validator
+    final isValid = _form.currentState!.validate();
+    if (isValid) {
+      _form.currentState!.save();
+      // later will send to firebase instead of printing it.
+      print(_enteredEmail);
+      print(_eneteredPassword);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.primary,
       body: Center(
         child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                margin: const EdgeInsets.only(
-                  top: 20,
-                  bottom: 20,
-                  right: 20,
-                  left: 20,
-                ),
-                width: 200,
-                child: Image.asset('lib/chat.png'),
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              margin: const EdgeInsets.only(
+                top: 20,
+                bottom: 20,
+                right: 20,
+                left: 20,
               ),
-              Card(
-                margin: const EdgeInsets.all(20),
-                child: SingleChildScrollView(
-                  child: Padding(padding: const EdgeInsets.all(16),
+              width: 200,
+              child: Image.asset('lib/chat.png'),
+            ),
+            Card(
+              margin: const EdgeInsets.all(20),
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(16),
                   child: Form(
+                    key: _form,
                     child: Column(
                       mainAxisSize: MainAxisSize.min,
                       children: [
                         TextFormField(
-                          decoration: const InputDecoration(
-                            labelText: 'Email Address'
-                          ),
+                          decoration:
+                              const InputDecoration(labelText: 'Email Address'),
                           keyboardType: TextInputType.emailAddress,
                           autocorrect: false,
                           textCapitalization: TextCapitalization.none,
+                          validator: (value) {
+                            if (value == null ||
+                                value.trim().isEmpty ||
+                                !value.contains('@')) {
+                              return 'Please enter a valid email address.';
+                            }
+                            return null;
+                          },
+                          onSaved: (value) {
+                            _enteredEmail = value!;
+                          },
                         ),
                         TextFormField(
-                          decoration: const InputDecoration(
-                            labelText: 'Password'
-                          ),
-                          obscureText: true,// hides the character after they are entered
+                          decoration:
+                              const InputDecoration(labelText: 'Password'),
+                          obscureText:
+                              true, // hides the character after they are entered
+                          validator: (value) {
+                            if (value == null || value.trim().length < 6) {
+                              return 'Password must be atleast 6 characters long.';
+                            }
+                            return null;
+                          },
+                          onSaved: (value) {
+                            _eneteredPassword = value!;
+                          },
                         ),
-                      ],),
+                        const SizedBox(
+                          height: 12,
+                        ),
+                        ElevatedButton(
+                          // all the data collected and sent to firebase from here
+                          onPressed: _sumbit,
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor:
+                                Theme.of(context).colorScheme.primaryContainer,
+                          ),
+                          child: Text(
+                            _isLogin ? 'Login' : 'Sign Up',
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            setState(() {
+                              _isLogin =
+                                  !_isLogin; // if already true then it will be false
+                            });
+                          },
+                          child: Text(_isLogin
+                              ? 'Create an acccount'
+                              : 'Already have an account.Login'),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-                ),
-          )],
-          )
-        ),
+              ),
+            )
+          ],
+        )),
       ),
     );
   }
